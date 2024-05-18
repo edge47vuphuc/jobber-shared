1349c5a5fe3a5897155b032945b58656ba228e3b
vuphucEgde47 <phucvu.edge.47@gmail.com>
2024-05-18 17:18:36 +0700

    added more interface

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+export {
+  IAuth,
+  IAuthBuyerMessageDetails,
+  IAuthDocument,
+  IAuthPayload,
+  IAuthResponse,
+  IAuthUser,
+  IEmailMessageDetails,
+  IForgotPassword,
+  IReduxAddAuthUser,
+  IReduxAuthPayload,
+  IReduxLogout,
+  IResetPassword,
+  ISignInPayload,
+  ISignUpPayload,
+} from './auth.interface';
+export { IBuyerDocument, IReduxBuyer } from './buyer.interface';
+export {
+  IChatBoxProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+  IChatSellerProps,
+  IConversationDocument,
+  IMessageDetails,
+  IMessageDocument,
+} from './chat.interface';
+export { IEmailLocals } from './email.interface';
+export {
+  GigType,
+  ICreateGig,
+  IGigCardItems,
+  IGigContext,
+  IGigInfo,
+  IGigTopProps,
+  IGigViewReviewsProps,
+  IGigsProps,
+  ISelectedBudget,
+  ISellerGig,
+} from './gig.interface';
+export {
+  IDeliveredWork,
+  IExtendedDelivery,
+  IOffer,
+  IOrderDocument,
+  IOrderEvents,
+  IOrderMessage,
+  IOrderNotifcation,
+  IOrderReview,
+} from './order.interface';
+export {
+  IRatingCategories,
+  IRatingCategoryItem,
+  IRatingTypes,
+  IReviewDocument,
+  IReviewMessageDetails,
+} from './review.interface';
+export {
+  IHitsTotal,
+  IPaginateProps,
+  IQueryList,
+  IQueryString,
+  ISearchResult,
+  ITerm,
+} from './search.interface';
+export {
+  ICertificate,
+  IEducation,
+  IExperience,
+  ILanguage,
+  ISellerDocument,
+  SellerType,
+} from './seller.interface';
